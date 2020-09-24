c148cb8cf34c3ab97244f6390c9b74d8ac01e5fc
m-dantas <mauricio.dantascp@gmail.com>
2020-09-24 10:43:52 -0300

    feat: inclui JTableRow

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,11 @@
 module.exports = {
   "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../components/**/*.stories.mdx",
+    "../components/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
+    "@storybook/addon-actions",
     "@storybook/addon-essentials"
   ]
 }
